e984cadc8f880d1008bebbb616071a82a1159ef0
= <=>
2024-03-01 16:01:55 +0530

    v1

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,22 +8,22 @@ git pull origin master
 echo "New changes copied to server !"
 
 # Activate Virtual Env
-source /home/django-server/PRODUCTION_ENV/bin/activate
-echo "Virtual env 'PRODUCTION_ENV' Activated !"
+# source /home/django-server/PRODUCTION_ENV/bin/activate
+# echo "Virtual env 'PRODUCTION_ENV' Activated !"
 
-echo "Installing Dependencies..."
-pip install -r requirements.txt --no-input
+# echo "Installing Dependencies..."
+# pip install -r requirements.txt --no-input
 
-echo "Serving Static Files..."
-python manage.py collectstatic --noinput
+# echo "Serving Static Files..."
+# python manage.py collectstatic --noinput
 
-echo "Running Database migration"
+# echo "Running Database migration"
 # python manage.py makemigrations
 # python manage.py migrate
 
 # Deactivate Virtual Env
-deactivate
-echo "Virtual env 'PRODUCTION_ENV' Deactivated !"
+# deactivate
+# echo "Virtual env 'PRODUCTION_ENV' Deactivated !"
 
 # Restart Server
 echo "Restarting Apache..."
