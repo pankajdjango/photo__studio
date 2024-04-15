e7131e0231dab4e270ac8876a7bae71c3fc727d7
= <=>
2024-04-15 10:22:09 +0530

    show message after booked request.

--- home/views.py
+++ home/views.py
@@ -4,38 +4,36 @@ from django.http import HttpResponse
 from ps_webapp.models import EventStatus, EventType, EventBookingMaster, AccountProfile
 from datetime import datetime
 from django.utils import timezone
+from django.contrib import messages
+
 # Create your views here.
 @login_required
 def index(request):
     context = dict()
+    messages_list = messages.get_messages(request)
+    context["message"] = messages_list
     return render(request, 'home/home.html',context)
 
-# "status" : EventStatus.objects.values_list('status',flat=True),
 
 @login_required
 def booknow(request):
     context = dict()
     if request.method=='POST':
         print(request.POST)
-        city_id = request.POST.get("city_id",None)  #['214784']
-        area_address = request.POST.get("area_address",None)    #['rews sdf']
-        mobile = request.POST.get("mobile",None)    #['7983456820']
-        message = request.POST.get("message",None)  #['test']
-        event_id = request.POST.get("event_id",None)    #['2']
-        event_date = request.POST.get("event_date",None)    #['2024-12-31T23:59']
-        userid=request.session.get('userid')
+        city_id = request.POST.get("city_id",None)
+        area_address = request.POST.get("area_address",None)
+        mobile = request.POST.get("mobile",None)
+        message = request.POST.get("message",None)
+        event_id = request.POST.get("event_id",None)
+        event_date = request.POST.get("event_date",None)
+        userid=request.session['userid']
         event_date_ist = datetime.strptime(event_date, '%Y-%m-%dT%H:%M').astimezone(timezone.get_current_timezone())
-        obj = EventBookingMaster(
-                city_id = city_id,
-                message = message,
-                event_date = event_date_ist,
-                mobile = mobile,
-                area_address = area_address,
-                event_id = event_id,
-                user_id = userid
-            )
-        obj.save()
-        print(obj.__dict__)
+        try:
+            EventBookingMaster(city_id=city_id,message=message,event_date=event_date_ist,mobile=mobile,area_address=area_address,event_id=event_id,user_id=userid).save()
+            messages.success(request, "Your Booking Request Sent Successfully.")
+        except Exception as e:
+            messages.error(request, "Something went wrong. Please try again.")
+        return redirect('/', **context)
     context["event_type"] = EventType.objects.values()
     return render(request, 'home/booknow.html',context)
 
