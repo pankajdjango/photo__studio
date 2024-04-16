828525cbfe8022ce968ecdb0329c1c19fd582623
pankajdjango <pankaj.django@gmail.com>
2024-04-16 17:38:43 +0530

    correct datetime IST format

--- home/templatetags/custom_filter.py
+++ home/templatetags/custom_filter.py
@@ -1,6 +1,8 @@
 from ps_webapp.models import CityMaster
 from django import  template
 from django.db import connection
+from datetime import datetime
+import pytz
 
 register=template.Library()
 
@@ -14,17 +16,18 @@ def city_name(city_id):
 
 @register.filter(name='epoch_timestamp')
 def epoch_timestamp(generated):
-    if not generated:return ""
-    with connection.cursor() as cursor:
-        cursor.execute(f"select to_timestamp({generated})::timestamp")
-        rows , = cursor.fetchall()
-    return f"{rows[0]}"
+    if not generated:
+        return ""
+    timestamp = datetime.fromtimestamp(generated)
+    return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
 
 
 @register.filter(name='timestamp')
 def timestamp(event_date):
-    if not event_date:return ""
-    with connection.cursor() as cursor:
-        cursor.execute(f"SELECT (TIMESTAMP WITH TIME ZONE '{event_date}')::timestamp")
-        rows , = cursor.fetchall()
-    return f"{rows[0]}"
+    if not event_date:
+        return ""
+    ist_timezone = pytz.timezone('Asia/Kolkata')
+    event_date_ist = event_date.astimezone(ist_timezone)
+    formatted_date = event_date_ist.strftime("%Y-%m-%d %H:%M:%S")
+    return formatted_date
